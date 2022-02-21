ceaf17dfa191a4e53965589d990c5f86983537d2
Adrian <48665902+acmatias@users.noreply.github.com>
2022-02-21 11:51:56 +1100

    html change

--- src/script.js
+++ src/script.js
@@ -3,22 +3,51 @@ import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import gsap from 'gsap'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+import { getGPUTier } from 'detect-gpu'
+;(async () => {
+    const gpuTier = await getGPUTier()
+    console.log(gpuTier)
+})()
+
+import Stats from 'stats.js'
+
+/**
+ * Stats
+ */
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
 /**
  * Debug
  */
 const gui = new dat.GUI({ width: 400 })
-gui.show(false)
+let guiToggle = true
+gui.show(gui._hidden)
+gui.open(gui._closed)
+
+window.addEventListener('keypress', (e) => {
+    if ((e.key === 'h' || e.key === 'H') && guiToggle == false) {
+        gui.show(guiToggle)
+        guiToggle = true
+    } else if ((e.key === 'h' || e.key === 'H') && guiToggle == true) {
+        gui.show(guiToggle)
+        guiToggle = false
+    }
+})
 
 const parameters = {
     materialColor: '#ffeded',
     count: 5000,
 }
-
-// gui.addColor(parameters, 'materialColor').onChange(() => {
-//     material.color.set(parameters.materialColor)
-//     particlesMaterial.color.set(parameters.materialColor)
-// })
 
 /**
  * Base
@@ -29,6 +58,75 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update Composer
+    bloomComposer.setSize(sizes.width, sizes.height)
+    bloomComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    finalComposer.setSize(sizes.width, sizes.height)
+    finalComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(-3.32, 0, 5.154)
+camera.rotation.set(0, 0, 0)
+cameraGroup.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    // alpha: true,
+    antialias: true,
+    // powerPreference: 'high-performance',
+})
+renderer.autoClear = false
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 0.45
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(2).step(0.001)
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+})
+
 const updateAllMaterials = () => {
     scene.traverse((child) => {
         if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
@@ -36,6 +134,7 @@ const updateAllMaterials = () => {
             child.material.envMapIntensity = 2.5
             child.castShadow = true
             child.receiveShadow = true
+            // scene.getObjectByName('Sphere002');
         }
     })
 }
@@ -89,32 +188,47 @@ gltfLoader.load('/models/astronaught.glb', (gltf) => {
     gltf.scene.position.set(-23.65, -4.095, -2.139)
     gltf.scene.rotation.set(0.16, 0.692, 0)
     astroGroup.add(gltf.scene)
-
-    gui.add(gltf.scene.rotation, 'x').min(-Math.PI).max(Math.PI).step(0.001).name('AstroRotationX')
-    gui.add(gltf.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('AstroRotationY')
-    gui.add(gltf.scene.position, 'x').min(-150).max(150).step(0.001).name('posX')
-    gui.add(gltf.scene.position, 'y').min(-150).max(150).step(0.001).name('posY')
-    gui.add(gltf.scene.position, 'z').min(-150).max(150).step(0.001).name('posZ')
     updateAllMaterials()
 })
 astroGroup.scale.set(0.3, 0.3, 0.3)
 astroGroup.position.set(-3.5, -2, 0)
 astroGroup.rotation.set(0.201, -0.536, -25)
 
+const lightBulbGroup = new THREE.Group()
+scene.add(lightBulbGroup)
+
+gltfLoader.load('/models/lightBulb.glb', (gltf) => {
+    lightBulbGroup.scale.set(0.3, 0.3, 0.3)
+    lightBulbGroup.position.set(-3.5, -2, 0)
+    lightBulbGroup.rotation.x = 0.2
+    lightBulbGroup.rotation.y = -0.291
+    lightBulbGroup.add(gltf.scene)
+
+    // const filament = scene.getObjectByName('Filament002')
+    // console.log(filament)
+
+    const light = scene.getObjectByName('Point002_Orientation')
+    light.position.y = 1
+    light.intensity = 50
+    gui.add(light, 'intensity').min(0).max(50).step(0.001).name('bulb light')
+
+    updateAllMaterials()
+})
+lightBulbGroup.scale.set(0.3, 0.3, 0.3)
+lightBulbGroup.position.set(-3.5, -2, 0)
+lightBulbGroup.rotation.set(0.2, -0.291, -25)
+
+const bulbGroup = new THREE.Group()
+scene.add(bulbGroup)
+
 gltfLoader.load('/models/astronaughtScene.glb', (gltf) => {
-    gltf.scene.scale.set(0.3, 0.3, 0.3)
-    gltf.scene.position.set(-3.5, -2, 0)
-    gltf.scene.rotation.x = 0.2
-    gltf.scene.rotation.y = -0.291
-    scene.add(gltf.scene)
+    bulbGroup.scale.set(0.3, 0.3, 0.3)
+    // gltf.scene.layers.set(1)
+    bulbGroup.position.set(-3.5, -2, 0)
+    bulbGroup.rotation.x = 0.2
+    bulbGroup.rotation.y = -0.291
+    bulbGroup.add(gltf.scene)
 
-    console.log(gltf.scene)
-
-    gui.add(gltf.scene.rotation, 'x').min(-Math.PI).max(Math.PI).step(0.001).name('AstroRotationX')
-    gui.add(gltf.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('AstroRotationY')
-    gui.add(gltf.scene.position, 'x').min(-150).max(150).step(0.001).name('posX')
-    gui.add(gltf.scene.position, 'y').min(-150).max(150).step(0.001).name('posY')
-    gui.add(gltf.scene.position, 'z').min(-150).max(150).step(0.001).name('posZ')
     updateAllMaterials()
 })
 
@@ -122,145 +236,74 @@ gltfLoader.load('/models/astronaughtScene.glb', (gltf) => {
  * Particles
  */
 //  -------------------------------------------------------------------------
-// Geometry
-const particlesGeometry = new THREE.BufferGeometry()
+// galaxy geometry
+const starGeometry = new THREE.SphereGeometry(80, 64, 64)
 
-const positions = new Float32Array(parameters.count * 3)
-const colors = new Float32Array(parameters.count * 3)
+// galaxy material
+const starMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load('textures/maps/galaxy1.png'),
+    side: THREE.BackSide,
+    transparent: true,
+})
 
-for (let i = 0; i < parameters.count; i++) {
-    positions[i] = (Math.random() - 0.5) * 50
-    positions[i + 1] = 0
-    positions[i + 2] = 0
-    colors[i] = Math.random()
-}
+// galaxy mesh
+const starMesh = new THREE.Mesh(starGeometry, starMaterial)
+// starMesh.layers.set(0)
+scene.add(starMesh)
 
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-// Material
-const particlesMaterial = new THREE.PointsMaterial()
-particlesMaterial.size = 0.1
-particlesMaterial.sizeAttenuation = true
-// particlesMaterial.color = new THREE.Color('#ff88cc');
-particlesMaterial.transparent = true
-particlesMaterial.alphaMap = particleTexture
-particlesMaterial.alphaTest = 0.001
-particlesMaterial.depthTest = false
-particlesMaterial.depthWrite = false
-particlesMaterial.blending = THREE.AdditiveBlending
-particlesMaterial.vertexColors = true
-
-// Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
-// gui.add(parameters, 'count').min(10000).max(100000).step(1000)
-// .onChange(() => {
-//     parameters.count = parameters.count
-// })
 //  -------------------------------------------------------------------------
+
+/**
+ * Bloom Pass
+ */
+// const renderScene = new RenderPass(scene, camera)
+// const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight))
+// bloomPass.threshold = 0.3
+// bloomPass.strength = 0.3
+// bloomPass.radius = 0
+
+// const bloomComposer = new EffectComposer(renderer)
+// bloomComposer.setSize(window.innerWidth, window.innerHeight)
+// bloomComposer.renderToScreen = false
+// bloomComposer.addPass(renderScene)
+// bloomComposer.addPass(bloomPass)
+
+// const finalPass = new ShaderPass(
+//     new THREE.ShaderMaterial({
+//         uniforms: {
+//             baseTexture: { value: null },
+//             bloomTexture: { value: bloomComposer.renderTarget2.texture },
+//         },
+//         vertexShader: vertexShader,
+//         fragmentShader: fragmentShader,
+//         defines: {},
+//     }),
+//     'baseTexture'
+// )
+// finalPass.needsSwap = true
+
+// const finalComposer = new EffectComposer(renderer)
+// finalComposer.addPass(renderScene)
+// finalComposer.addPass(finalPass)
+
+// gui.add(bloomPass, 'threshold').min(-0).max(5).step(0.001).name('threshold')
+// gui.add(bloomPass, 'strength').min(-0).max(5).step(0.001).name('strength')
+// gui.add(bloomPass, 'radius').min(-0).max(5).step(0.001).name('radius')
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight('#ffffff', 0)
-directionalLight.position.set(0.25, 2.601, 5)
-directionalLight.castShadow = true
-directionalLight.shadow.camera.far = 10
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.normalBias = 0.015
-scene.add(directionalLight)
-
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('moonLightIntensity')
-gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('moonLightX')
-gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('moonLightY')
-gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('moonLightZ')
-gui.add(directionalLight.shadow, 'normalBias').min(0).max(0.1).step(0.0001)
-
-const pointLight = new THREE.PointLight('#E79B58', 20)
-pointLight.position.set(-0.605, -0.214, 0.177)
+const pointLight = new THREE.PointLight('#E79B58', 10)
+pointLight.position.set(-0.409, -0.605, 0.373)
 pointLight.castShadow = true
 pointLight.shadow.camera.far = 5
 pointLight.shadow.mapSize.set(1024, 1024)
 pointLight.shadow.normalBias = 0.015
-gui.add(pointLight, 'intensity').min(0).max(100).step(0.001).name('lightIntensity')
-gui.add(pointLight.position, 'x').min(-15).max(15).step(0.001).name('pointLightX')
-gui.add(pointLight.position, 'y').min(-15).max(15).step(0.001).name('pointLightY')
-gui.add(pointLight.position, 'z').min(-15).max(15).step(0.001).name('pointLightZ')
+gui.add(pointLight, 'intensity').min(0).max(100).step(0.001).name('crystal light')
 scene.add(pointLight)
-
-// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-// scene.add(directionalLightHelper)
-// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
-// scene.add(pointLightHelper)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-}
-
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Group
-const cameraGroup = new THREE.Group()
-scene.add(cameraGroup)
-
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(-3.32, 0, 5.154)
-camera.rotation.set(0, 0, 0)
-cameraGroup.add(camera)
-
-gui.add(camera.position, 'x').min(-50).max(50).step(0.001).name('camera posX')
-gui.add(camera.position, 'y').min(-50).max(50).step(0.001).name('camera posY')
-gui.add(camera.position, 'z').min(-50).max(50).step(0.001).name('camera posZ')
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
-    antialias: true,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.3
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-
-gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
-gui.add(renderer, 'toneMapping', {
-    No: THREE.NoToneMapping,
-    Linear: THREE.LinearToneMapping,
-    Reinhard: THREE.ReinhardToneMapping,
-    Cineon: THREE.CineonToneMapping,
-    ACESFilmic: THREE.ACESFilmicToneMapping,
-})
 
 /**
  * Scroll
@@ -276,18 +319,13 @@ window.addEventListener('scroll', () => {
 
     if (newSection != currentSection) {
         currentSection = newSection
-        // gsap.to(sectionMeshes[currentSection].rotation, {
-        //     duration: 1.5,
-        //     ease: 'power2.inOut',
-        //     x: '+=6',
-        //     y: '+=3',
-        //     z: '+=1.5',
-        // })
-        // if (currentSection === 1) {
-
-        //     gsap.to(camera.position, { duration: 0.5, ease: 'power2.inOut', z: 1.894 })
-        //     gsap.to(camera.position, { duration: 0.5, ease: 'power2.inOut', z: 1.894 })
-        // }
+        gsap.to(sectionMeshes[currentSection].rotation, {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            x: '+=6',
+            y: '+=3',
+            z: '+=1.5',
+        })
     }
 })
 
@@ -310,6 +348,7 @@ const clock = new THREE.Clock()
 let previousTime = 0
 
 const tick = () => {
+    stats.begin()
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
@@ -332,10 +371,13 @@ const tick = () => {
     astroGroup.position.y = Math.cos(previousTime) * 0.2
 
     // Render
+    // bloomComposer.render()
+    // finalComposer.render()
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+    stats.end()
 }
 
 tick()
